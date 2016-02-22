a01f90b5879b3ea0b9657239d8725e0632703693
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-02-22 12:41:49 +0100

    account and token overview

--- client/myDapp.js
+++ client/myDapp.js
@@ -1,23 +1,35 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+        return Session.get('counter');
     }
-  });
+});
 
-  Template.hello.events({
+Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
     }
-  });
-}
+});
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.accounts.helpers({
+    listAccounts: function () {
+        return EthAccounts.find().fetch();
+    },
+    latestBlock: function () {
+        return EthBlocks.latest
+    }
+});
+
+
+Template.accountDetails.onRendered(function () {
+    var template = Template.instance();
+    var tokenInstance = TokenContract.at("0x938564f4736b18064d4e03ed2da235380ae48890");
+    var templateAddress = Template.currentData().address;
+    tokenInstance.coinBalanceOf(templateAddress, function (err, balance) {
+        TemplateVar.set(template, "currentTokenBalance", balance);
+    });
+});
