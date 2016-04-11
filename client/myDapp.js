26674d92540243452677e3af09ce1060d5c96495
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-11 19:01:47 +0200

    removed account info, and added click event on tooth-class

--- client/myDapp.js
+++ client/myDapp.js
@@ -29,8 +29,18 @@ Template.accountDetails.onRendered(function () {
     var template = Template.instance();
     var tokenInstance = TokenContract.at("0x938564f4736b18064d4e03ed2da235380ae48890");
     var templateAddress = Template.currentData().address;
-    
+
     tokenInstance.coinBalanceOf(templateAddress, function (err, balance) {
         TemplateVar.set(template, "currentTokenBalance", balance);
     });
+});
+
+Template.dentition.events({
+  'click .tooth': function(e) {
+    if (_.contains(e.target.classList, 'tooth-fill')){
+      e.target.classList.remove('tooth-fill');
+    } else {
+      e.target.classList.add('tooth-fill');
+    }
+  }
 });
