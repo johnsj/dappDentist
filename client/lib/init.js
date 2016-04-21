73cbd95aa7ec786958426401d57ab3dcc1618c04
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-21 07:44:03 +0200

    updated meteor, added meteor npm and added ipfs

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,5 +1,9 @@
-// if(typeof web3 === 'undefined')
-//     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-//
-//     EthBlocks.init();
-//     EthAccounts.init();
+import IPFS from 'ipfs-api';
+
+if(typeof web3 === 'undefined')
+    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+EthBlocks.init();
+EthAccounts.init();
+
+ipfs = IPFS({host: 'localhost', port: '5001', procotol: 'http'});
