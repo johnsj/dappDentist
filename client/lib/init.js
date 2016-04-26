4d93ba1e365d5aff1873a33dcd368f8bc644ec8d
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-26 18:34:06 +0200

    connection to IPFS and WEB3 working

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,12 +1,13 @@
-// import IPFS from 'ipfs-api';
-//
+import ipfsAPI from 'ipfs-js';
+
 if(typeof web3 === 'undefined')
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-//
+
 EthBlocks.init();
 EthAccounts.init();
 
-// ipfs = IPFS({host: 'localhost', port: '5001', procotol: 'http'});
+window.ipfs = ipfsAPI;
+window.ipfs.setProvider();
 
 $(document).ready(function() {
   $(".button-collapse").sideNav();
