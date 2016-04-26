bb0aa885923656379e1c3674fd423cd7adc1c279
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-26 18:34:34 +0200

    forgot to add patient.js file

---
+++ client/patient/patient.js
@@ -0,0 +1,39 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import classnames from 'classnames';
+
+Template.PatientCard.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.set('visitComplete', false);
+});
+
+Template.PatientCard.helpers({
+  isVisitComplete(){
+    return Template.instance().state.get('visitComplete');
+  },
+
+  visitCSSClass(){
+    let classname = classnames({
+      red: !Template.instance().state.get('visitComplete'),
+      green: Template.instance().state.get('visitComplete')
+    })
+
+    return classname;
+  }
+});
+
+Template.PatientCard.events({
+  'click .btn-complete-visit'(event, instance) {
+    event.preventDefault();
+    instance.state.set('visitComplete',!instance.state.get('visitComplete'));
+    // console.log(ipfs);
+    ipfs.api.id(function(err,res){
+      if (err) {
+        console.log(err);
+        return
+      };
+      console.log(res);
+    });
+  }
+});
