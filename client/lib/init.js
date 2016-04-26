4cb3b50ef464a0685f7a67541a2419190836890f
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-26 14:34:55 +0200

    style work

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,9 +1,13 @@
-import IPFS from 'ipfs-api';
-
+// import IPFS from 'ipfs-api';
+//
 if(typeof web3 === 'undefined')
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
+//
 EthBlocks.init();
 EthAccounts.init();
 
-ipfs = IPFS({host: 'localhost', port: '5001', procotol: 'http'});
+// ipfs = IPFS({host: 'localhost', port: '5001', procotol: 'http'});
+
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+})
