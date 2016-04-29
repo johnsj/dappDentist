153cc02d8e43387424ba87cf9a3e5fcf2cd0db5f
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-29 23:35:41 +0200

    added path.resolve to make settingsfile obsolete

--- server/upload/upload.js
+++ server/upload/upload.js
@@ -1,4 +1,6 @@
-var imageStore = new FS.Store.FileSystem("images", {path: Meteor.settings.private.uploadPath});
+import path from 'path';
+
+var imageStore = new FS.Store.FileSystem("images", {path: path.resolve('.')+'/uploads'});
 
 Images = new FS.Collection("images", {
   stores: [imageStore]
