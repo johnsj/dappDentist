be340da9bd37378b51cb9fb48f37f1829adb379b
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-04-30 18:26:38 +0200

    comments are now saved to db

--- client/dentition/comments/comments.js
+++ client/dentition/comments/comments.js
@@ -1,31 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
+import { Comments } from '../../../collections/comments.js';
 
-//Temporary array for UI prototyping
-let allComments = [
-  {
-    doctor: 'Dr. Feelgood',
-    date: new Date(),
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere nibh non purus lacinia, in interdum lectus finibus. Nulla facilisi. Curabitur tincidunt bibendum turpis, vitae semper mi. Praesent viverra tortor a nisi rutrum, eu hendrerit nulla tincidunt. Donec gravida sagittis bibendum. Maecenas eu odio suscipit, gravida lectus id, fringilla dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum et nulla at lorem semper condimentum. Nam ligula ex, pretium ac tempor sed, aliquam vel tellus. Aenean ornare urna at nulla bibendum efficitur. Nam ac risus quis felis blandit semper ac in sem. Vestibulum tempor leo id risus placerat, nec pretium massa porttitor. Suspendisse auctor justo vitae congue faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra tincidunt risus at rhoncus. Aenean convallis ex sit amet commodo commodo. Mauris ut gravida nisl. Vestibulum commodo leo est, in scelerisque ante mollis sed. Maecenas eu dignissim."
-  },
-  {
-    doctor: 'Dr. Feelgood',
-    date: new Date(),
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere nibh non purus lacinia, in interdum lectus finibus. Nulla facilisi. Curabitur tincidunt bibendum turpis, vitae semper mi. Praesent viverra tortor a nisi rutrum, eu hendrerit nulla tincidunt. Donec gravida sagittis bibendum. Maecenas eu odio suscipit, gravida lectus id, fringilla dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum et nulla at lorem semper condimentum. Nam ligula ex, pretium ac tempor sed, aliquam vel tellus. Aenean ornare urna at nulla bibendum efficitur. Nam ac risus quis felis blandit semper ac in sem. Vestibulum tempor leo id risus placerat, nec pretium massa porttitor. Suspendisse auctor justo vitae congue faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra tincidunt risus at rhoncus. Aenean convallis ex sit amet commodo commodo. Mauris ut gravida nisl. Vestibulum commodo leo est, in scelerisque ante mollis sed. Maecenas eu dignissim."
-  },
-  {
-    doctor: 'Dr. Feelgood',
-    date: new Date(),
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere nibh non purus lacinia, in interdum lectus finibus. Nulla facilisi. Curabitur tincidunt bibendum turpis, vitae semper mi. Praesent viverra tortor a nisi rutrum, eu hendrerit nulla tincidunt. Donec gravida sagittis bibendum. Maecenas eu odio suscipit, gravida lectus id, fringilla dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum et nulla at lorem semper condimentum. Nam ligula ex, pretium ac tempor sed, aliquam vel tellus. Aenean ornare urna at nulla bibendum efficitur. Nam ac risus quis felis blandit semper ac in sem. Vestibulum tempor leo id risus placerat, nec pretium massa porttitor. Suspendisse auctor justo vitae congue faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra tincidunt risus at rhoncus. Aenean convallis ex sit amet commodo commodo. Mauris ut gravida nisl. Vestibulum commodo leo est, in scelerisque ante mollis sed. Maecenas eu dignissim."
-  },
-];
+let current_patient_id = FlowRouter.getParam('patient_id');
 
-Template.Comments.helpers({
+Template.CommentsList.helpers({
   comments(){
-    //TODO
-    //add logic to retrieve comments from DB
-    return allComments;
+    return Comments.find({patient_id: current_patient_id},{sort:{date:-1}});
   }
 })
 
@@ -34,13 +16,13 @@ Template.CommentsForm.events({
     events.preventDefault();
 
     let newElement = {
+      patient_id: current_patient_id,
       doctor: 'Dr. Feelgood',
       date: new Date(),
       text: $('#journalText').val()
     }
 
-    //TODO
-    //Add logic for adding comment to DB
+    Comments.insert(newElement);
 
     $('#journalText').val('');
     $('#journalText').trigger('autoresize');
