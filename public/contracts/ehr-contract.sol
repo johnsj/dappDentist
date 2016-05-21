cb50021b1928714cf85c76610d539497a58a0d3b
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-05-21 16:30:44 +0200

    first draft at a functioning but simple contract

--- public/contracts/ehr-contract.sol
+++ public/contracts/ehr-contract.sol
@@ -5,3 +5,23 @@ contract EHRdk{
     RecordsRequested(msg.sender, patient);
   }
 }
+
+contract journalDb {
+
+  event journalUpdated(address patient);
+
+  mapping (address => string) journals;
+
+  function getJournal(address patient) {
+    if(journals[patient] != ""){
+      return journals[patient];
+    }
+    return false;
+  }
+
+  function updateJournal(address patient, string hash) {
+    journals[patient] = hash;
+    journalUpdated(patient);
+  }
+
+}
