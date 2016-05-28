c0ff71851d8a7197a2209bf498e0b602af512766
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-05-28 21:26:54 +0200

    added folder check

--- server/startup.js
+++ server/startup.js
@@ -1,8 +1,25 @@
 import { Web3 } from 'meteor/ethereum:web3';
+import fs from 'fs';
+import {archiveLocation, archiveDownLocation, uploadLocation} from './const.js';
 
 Meteor.startup(()=>{
+
+  fs.stat(archiveLocation,(err, stat) => {
+    if (err) {
+      fs.mkdirSync(archiveLocation);
+    }
+  });
+  fs.stat(archiveDownLocation,(err, stat) => {
+    if (err) {
+      fs.mkdirSync(archiveDownLocation);
+    }
+  });
+
+
   if(typeof web3 === 'undefined') {
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
   }
   web3.eth.defaultAccount = Meteor.settings.private.coinbase;
+
+
 })
