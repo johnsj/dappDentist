4cbe6c9a2cd433046f74abd3289483f3693bc8f5
John Schwartz Jacobsen <john@johnschwartz.dk>
2016-05-29 13:55:14 +0200

    fixed path ending in const.js

--- server/const.js
+++ server/const.js
@@ -1,5 +1,5 @@
 import path from 'path';
 
-export const archiveLocation = path.resolve(Meteor.absolutePath + '/../archives~/');
-export const archiveDownLocation = path.resolve(Meteor.absolutePath + '/../archives~/tmp~/');
+export const archiveLocation = path.resolve(Meteor.absolutePath + '/../archives~/') +"/";
+export const archiveDownLocation = path.resolve(Meteor.absolutePath + '/../archives~/tmp~/') +"/";
 export const uploadLocation = Meteor.absolutePath + '/uploads/';
